9cac1e5a372aa5126869c693339a962b6996d8fc
JuanPablo <JuanPablo@192.168.1.12>
2017-09-20 22:18:20 -0500

    Adelantos en punto 1 4 5 de la practica, sin detalles muy tecnicos, tecnicamente listo, realizar modificaciones para eventuales errores futuros.

--- Teoria/src/teoria/Gramatica.java
+++ Teoria/src/teoria/Gramatica.java
@@ -6,6 +6,7 @@
 package teoria;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import javax.swing.JOptionPane;
 
@@ -15,44 +16,49 @@ import javax.swing.JOptionPane;
  */
 public class Gramatica {
 
-    ArrayList<Integer> posArray = new ArrayList<Integer>();
+    public ArrayList<Integer> posArray = new ArrayList<Integer>();
+    public ArrayList<Integer> posAlcanzablesArray = new ArrayList<Integer>();
+    public String NVivos;
+    public String NMuertos;
+    public String NInalcanzables;
+    public String Nuevaimpresion;
 
     public Gramatica() {
     }
 
-    public Gramatica(String archivo) {
+   /**
+    * METODO EL CUAL CREA UN ARREGLO CON LOS ELEMENTOS DE LA GRAMATICA
+    * @param archivo
+    * @return 
+    */
+    public String[] Gramatica(String archivo) {
 
-        String[] primertoken = archivo.split("\n");
-        String[] tokenfinal = null;
-        ArrayList<String> arreglotokenizado = new ArrayList<>();
-
-        int var = 0;
-        for (int i = 0; i < primertoken.length; i++) {
-            String palabra = primertoken[i];
-            tokenfinal = palabra.split("=");
+        String[] primertoken = archivo.split("\n");//arreglo con cada reglos del texto(el cual tiene la gramatica)
+        String[] tokenfinal = null;// arreglo que tendra de cada renglon el string antes y despues del =
+        ArrayList<String> arreglotokenizado = new ArrayList<>(); //arraylist para almacenar todos os elementos separados      
+        for (int i = 0; i < primertoken.length; i++) { 
+            String palabra = primertoken[i]; //String que almacena la palabra i del primer arreglo
+            tokenfinal = palabra.split("="); 
             for (int j = 0; j < tokenfinal.length; j++) {
-                String palabra1 = tokenfinal[j];
+                String palabra1 = tokenfinal[j]; //String que almacena la palabra j del segundo arreglo
                 arreglotokenizado.add(palabra1);
             }
-
         }
+        String[] Gramaticatokenizada = arreglotokenizado.stream().toArray(String[]::new);       
 
-        String[] Gramaticatokenizada = arreglotokenizado.stream().toArray(String[]::new);
-
-//        for (int i = 0; i < Gramaticatokenizada.length; i++) {
-//            System.out.println("Posicion: " +i + "caracater: " + Gramaticatokenizada[i]);
-//        }
-        Vivos(Gramaticatokenizada);
-        Muertos(Vivos(Gramaticatokenizada), Gramaticatokenizada);
+        return(Gramaticatokenizada);
     }
 
+    /**
+     *
+     * @param gramatica
+     * @return
+     */
     public String Vivos(String[] gramatica) {
 
         ArrayList<String> TV = new ArrayList<>();
         ArrayList<String> NVivos = new ArrayList<>();
-
         ArrayList<Integer> actual = new ArrayList<Integer>();
-
         String Nvivos = "";
         boolean hayVivos = false;
 
@@ -70,12 +76,10 @@ public class Gramatica {
                     Nvivos = Nvivos.concat(Nviv);
                     hayVivos = true;
                 }
-
             }
         }
 
         if (hayVivos == false) {
-
             JOptionPane.showMessageDialog(null, "NO HAY VIVOS NI MUERTOS");
         }
 
@@ -103,35 +107,32 @@ public class Gramatica {
 
                             }
                         }
-
                     }
                     if (continuar == true) {
                         String Nviv = gramatica[i - 1];
                         boolean esta = false;
+                        actual.add(i);
+                        TV.add(posicion);
+                        NVivos.add(Nviv);
+                        posArray.add(i - 1);
                         if (!Nvivos.contains(Nviv)) {
-                            actual.add(i);
-                            TV.add(posicion);
-                            NVivos.add(Nviv);
-                            posArray.add(i - 1);
                             Nvivos = Nvivos.concat(Nviv);
                             hayVivos = true;
                         }
-
                     }
-
                 }
             }
         }
-        Iterator<String> nombreIterator = NVivos.iterator();
-//        while (nombreIterator.hasNext()) {
-//            String elemento = nombreIterator.next();
-//            System.out.print(elemento + " / ");
-//        }
 
         return (Nvivos);
-
     }
 
+    /**
+     *
+     * @param NVivos
+     * @param gramatica
+     * @return
+     */
     public String Muertos(String NVivos, String[] gramatica) {
         ArrayList<String> Nmuertos = new ArrayList<>();
         String Ntmuertos = "";
@@ -148,13 +149,98 @@ public class Gramatica {
             }
 
         }
-        Iterator<String> nombreIterator = Nmuertos.iterator();
-        while (nombreIterator.hasNext()) {
-            String elemento = nombreIterator.next();
-            System.out.print(elemento + " / ");
-        }
 
         return (Ntmuertos);
+    }
+
+    /**
+     *
+     * @param gramatica
+     * @return
+     */
+    public String inalcanzables(String[] gramatica) {
+        ArrayList<String> Alcanzables = new ArrayList<>();
+        ArrayList<String> Inalcanzables = new ArrayList<>();
+        String Ninalcanzables = "";
+        String Nalcanzables = "";
+
+        for (int i = 0; i < gramatica.length; i++) {
+            if (i % 2 != 0) {
+                String posicion = gramatica[i];
+                String[] porpos = posicion.split(">");
+                for (int j = 0; j < porpos.length - 1; j++) {
+                    String ps = porpos[j];
+                    ps = ps.trim();
+                    int signo = ps.indexOf("<");
+                    ps = ps.substring(signo, ps.length());
+                    int encontrado = Nalcanzables.indexOf(ps);
+                    if (encontrado == -1) {
+                        Nalcanzables = Nalcanzables.concat(ps).concat(">");
+
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < gramatica.length; i++) {
+            if (i % 2 == 0) {
+                String palabrai = gramatica[i];
+                if (Nalcanzables.contains(palabrai)) {
+                    Alcanzables.add(palabrai);
+                    posAlcanzablesArray.add(i);
+                } else {
+                    if (!Ninalcanzables.contains(palabrai)) {
+                        Ninalcanzables = Ninalcanzables.concat(palabrai);
+                        Inalcanzables.add(palabrai);
+                    }
+
+                }
+            }
+        }
+
+        return (Ninalcanzables);
+
+    }
+
+    /**
+     *
+     * @param posVivos
+     * @param posAlcanzables
+     * @param gramatica
+     * @return
+     */
+    public String impresion(ArrayList posVivos, ArrayList posAlcanzables, String[] gramatica) {
+        String Ngramatica = "";
+        Collections.sort(posVivos);
+        Collections.sort(posAlcanzables);
+        Integer[] posicionVivos = (Integer[]) posVivos.stream().toArray(Integer[]::new);
+        Integer[] posicionAcanzables = (Integer[]) posVivos.stream().toArray(Integer[]::new);
+
+        if (posVivos.isEmpty() == false) {
+            for (int i = 0; i < posicionVivos.length; i++) {
+                Integer uno = posicionVivos[i];
+                int x = uno.intValue();
+                for (int j = 0; j < posicionAcanzables.length; j++) {
+                    Integer dos = posicionAcanzables[j];
+                    int y = dos.intValue();
+                    String palabraprocesada = "";
+                    palabraprocesada = palabraprocesada.concat(gramatica[x]).concat("=").concat(gramatica[x + 1]).concat("\n");
+                    if (y == x && !Ngramatica.contains(palabraprocesada)) {
+                        Ngramatica = Ngramatica.concat(palabraprocesada);
+                    }
+                }
+            }
+
+            System.out.println(Ngramatica);
+        } else {
+            for (int i = 0; i < posicionAcanzables.length; i++) {
+                Integer dos = posicionAcanzables[i];
+                int y = dos.intValue();
+                String palabraprocesada = "";
+                palabraprocesada = palabraprocesada.concat(gramatica[y]).concat("=").concat(gramatica[y + 1]).concat("\n");
+            }
+        }
+        return (Ngramatica);
     }
 
 }
